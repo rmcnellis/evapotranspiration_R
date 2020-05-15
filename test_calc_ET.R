1a92c58cc8cddfe0031b269936f512de8c1b5417
Risa McNellis <risa.mcnellis@ttu.edu>
2020-05-15 15:26:08 -0500

    Update test_calc_ET.R

--- test_calc_ET.R
+++ test_calc_ET.R
@@ -2,13 +2,10 @@
 
 source('calc_ET.R')
 
-test_ET <- calc_ET(Pa = 90, Ta = 25, RH = 40, J = 180, l = 0.58, elev = 900, ns = 6, 
-                   alb = 0.2, Tmax = 30, Tmin = 20, L = 2, h = 0.3, ux = 2, x = 2, gl = 400, 
-                   SM = 0.1)
+test_dual_ET <- calc_dual_ET(Ta = seq(5, 35, 5))
 
 View(test_ET)
 
-test_ETs <- calc_ETs(Pa = 90, Ta = 25, RH = 40, J = 180, l = 0.58, elev = 900, ns = 6, 
-                     alb = 0.2, Tmax = 30, Tmin = 20, ux = 2, SM = 0.1)
+test_soil_ET <- calc_soil_ET(Ta = seq(5, 35, 5))
 
 View(test_ETs)
