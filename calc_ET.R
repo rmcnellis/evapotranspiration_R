bf2595a593d26be50c9032e3c8eb619da6b1e469
Risa McNellis <risa.mcnellis@ttu.edu>
2020-05-15 15:25:34 -0500

    Update calc_ET.R
    add comments

--- calc_ET.R
+++ calc_ET.R
@@ -75,6 +75,7 @@
 # Tmin: minimum air temperature (C)
 # Tw: wet bulb temperature (C)
 # ux: wind speed at reference height (m s-1)
+# Vm: volume of one mole (m3 mol-1)
 # ws: sunset hour angle (rad)
 # x: reference height (m)
 # y: psychrometer constant (kPa C-1)
@@ -90,18 +91,18 @@ library(bigleaf)
 
 # All equation numbers refer to equations presented in McNellis & Smith
 
-calc_ET <- function(y = 0.06, Pa, Ta, RH, J, l, elev, ns, as = 0.25, bs = 0.5, alb, 
-                    Tmax, Tmin, L, h, ux, x = 2, gl, Sc = 1, Pr = 1, SM, SMs = 0.5, 
-                    n = 2, zs = 0.004, Cp = 1005) {
+calc_dual_ET <- function(y = 0.06, Pa = 90, Ta = 25, RH = 40, J = 180, l = 0.58, elev = 900,
+                         ns = 6, as = 0.25, bs = 0.5, alb = 0.2, Tmax = 30, Tmin = 20, L = 2, 
+                         h = 0.3, ux = 2, x = 2, gl = 400, Sc = 1, Pr = 1, SM = 0.1, 
+                         SMs = 0.5, n = 2, zs = 0.004, Cp = 1005) {
   
   # constants
   Gsc <- 0.082
   k <- 0.41
   Rd <- 287.058
   Rv <- 461.495
-  n <- 2
 
-  # environmental terms
+  # calculate environmental terms
   D <- RHtoVPD(RH = RH, TdegC = Ta, Pa = Pa)
   Tw <- wetbulb.temp(Tair = Ta, pressure = Pa * 1000, VPD = D)
   esTw <- 0.6108 * exp((17.27 * Tw) / (Tw + 238.3))
@@ -113,60 +114,74 @@ calc_ET <- function(y = 0.06, Pa, Ta, RH, J, l, elev, ns, as = 0.25, bs = 0.5, a
   pd <- (Pa * 1000) - pv
   p <- (pd / (Rd * (Ta + 273.15))) + (pv / (Rv * (Ta + 273.15)))
   
-  # net radiation for location and day
+  # calculate distance from the earth to the sun, solar decimation, and sunset solar angle
   dr <- 1 + 0.033 * cos(J * ((2 * pi) / 365))
   sd <- 0.409 * sin(J * ((2 * pi) / 365) - 1.39)
   ws <- (pi/2) - atan((-tan(l) * tan(sd)) / (1 - ((tan(l))^2) * ((tan(sd))^2)^0.5))
+  # calculate extraterrestrial radiation
   Ra <- (24 * 60 / pi) * Gsc * dr * 
         (ws * sin(l) * sin(sd) + cos(l) * cos(sd) * sin(ws))
   N <- ws * (24 / pi)
+  # calculate shortwave radiation
   Rs <- ((as + bs * (ns / N)) * Ra)
   Rso <- (0.75 + (elev * 2e-5)) * Ra
+  # calculate net shortwave radiation
   Rns <- (1 - alb) * Rs
+  # calculate net longwave radiation
   Rnl <- (((Tmax + Tmin) / 2) * (0.34 - 0.14 * sqrt(ea)) * (1.35 * (Rs/Rso) - 0.35))
+  # calculate net radiation
   Rn_MJ <- (Rns + Rnl)
   # convert from MJ m-2 day-1 to W m-2
   Rn <- 11.82033 * Rn_MJ
   
-  # radiation terms, Eq. 22-23
+  # calculate Rsurf, G, A, As, Eq. 22-23
   Rsurf <- Rn * exp(-0.7 * L)
   G <- (Rsurf * 0.2)
   A <- 400 - G
   As <- Rsurf - G
   
-  # canopy terms
+  # calculate canopy terms d, zc, uf
   d <- h * 0.63
   zc <- h * 0.13
   uf <- (0.41 * ux) / (log((x - d) / zc)) #Eq. 5
   
-  # resistance terms
+  # calculate resistance terms
   rl <- 1 / (gl * Vm) #Eq. 1
   rc <- rl / (2 * L) #Eq. 2
   rb <- (2 * (0.41 * uf)^-1) * ((Sc^0.67) / Pr) #Eq. 3
   rv <- rb / (2 * L) #Eq. 4
   rs <- 1 /(exp(4.2555 * (SM / SMs) - 8.206)) #Eq. 6
-  rg_o <- ((log((x - d) / zc)) / ((k^2) * ux)) * 
-          (h / (n * (h - d))) * (exp(n) - exp(n * (1 - ((d + zc) / h)))) # Eq. 7
-  rg_i <- (log(x / zs) * log((d + zc) / zs)) / ((k^2) * ux) #Eq. 8
+    # rg at no canopy cover
+    rg_o <- ((log((x - d) / zc)) / ((k^2) * ux)) * 
+            (h / (n * (h - d))) * (exp(n) - exp(n * (1 - ((d + zc) / h)))) # Eq. 7
+    # rg at full canopy cover
+    rg_i <- (log(x / zs) * log((d + zc) / zs)) / ((k^2) * ux) #Eq. 8
+  # rg at given LAI
   rg <- ifelse (L <= 4, 0.25 * L * rg_o + 0.25 * (4 - L) * rg_i, rg_o) #Eq. 11-12
-  ra_o <- ((log((x - d) / zc)) / ((k^2) * ux)) * 
-          ((log((x - d) / (h - d))) + (h / (n * (h - d))) * 
-          (exp(n * (1 - ((d + zc) / h))) - 1)) #Eq. 9
-  ra_i <- ((log(x / zs))^2) / ((k^2) * ux) - rg_i #Eq. 10
+    # ra at no canopy cover
+    ra_o <- ((log((x - d) / zc)) / ((k^2) * ux)) * 
+            ((log((x - d) / (h - d))) + (h / (n * (h - d))) * 
+            (exp(n * (1 - ((d + zc) / h))) - 1)) #Eq. 9
+    # ra at full canopy cover
+    ra_i <- ((log(x / zs))^2) / ((k^2) * ux) - rg_i #Eq. 10
+  # ra at given LAI
   ra <- ifelse (L <= 4, 0.25 * L * ra_o + 0.25 * (4 - L) * ra_i, ra_o) #Eq. 11-12
   
-  # resistance coefficients, Eq. 13-17
+  # calculate aerodynamic, substrate, and canopy resistance coefficients, Eq. 13-15
   rA <- (s + y) * ra 
   rS <- (s + y) * rg + (y * rs)
   rC <- (s + y) * rv + (y * rc)
+  
+  # calculate canopy and substrate coefficients, Eq. 16-17
   Cc <- (1 + ((rC * rA) / (rS * (rC + rA)))) ^ -1
   Cs <- (1 + ((rS * rA) / (rC * (rS + rA)))) ^ -1
 
-  # evapotranspiration, Eq. 19-21
+  # calculate evapotranspiration using Penman-Monteith equations, Eq. 19-21
   PMc <- (s * A + (p * Cp * D - s * rv * As) / (ra + rv)) / 
          (s + y * (1 + (rc / (ra + rv))))
   PMs <- (s * A + (p * Cp * D - s * rg * (A - As)) / (ra + rg)) / 
          (s + y * (1 + rs / (ra + rg)))
+  # calculate total evapotranspiration from Penman-Monteith equations and coefficients
   ET <- Cc * PMc + Cs * PMs
   
   # output
@@ -175,19 +190,19 @@ calc_ET <- function(y = 0.06, Pa, Ta, RH, J, l, elev, ns, as = 0.25, bs = 0.5, a
 }
 
 # for bare soil only, Eq. 24
-calc_ETs <- function(y = 0.06, Pa, Ta, RH, J, l, elev, ns, as = 0.25, bs = 0.5, alb, 
-                     Tmax, Tmin, ux, x = 2, Sc = 1, Pr = 1, SM, SMs = 0.5, 
-                     n = 2, zs = 0.004, Cp = 1005) {
+calc_soil_ET <- function(y = 0.06, Pa = 90, Ta = 25, RH = 40, J = 180, l = 0.58, elev = 900, 
+                         ns = 6, as = 0.25, bs = 0.5, alb = 0.2, Tmax = 30, Tmin = 20, ux = 2, 
+                         x = 2, Sc = 1, Pr = 1, SM = 0.1, SMs = 0.5, n = 2, zs = 0.004, 
+                         Cp = 1005) {
   
   # constants
   Gsc <- 0.082
   k <- 0.41
   Rd <- 287.058
   Rv <- 461.495
-  n <- 2
   d <- 0
   
-  # environmental terms
+  # calculate environmental terms
   D <- RHtoVPD(RH = RH, TdegC = Ta, Pa = Pa)
   Tw <- wetbulb.temp(Tair = Ta, pressure = Pa * 1000, VPD = D)
   esTw <- 0.6108 * exp((17.27 * Tw) / (Tw + 238.3))
@@ -198,29 +213,34 @@ calc_ETs <- function(y = 0.06, Pa, Ta, RH, J, l, elev, ns, as = 0.25, bs = 0.5, 
   pd <- (Pa * 1000) - pv
   p <- (pd / (Rd * (Ta + 273.15))) + (pv / (Rv * (Ta + 273.15)))
   
-  # net radiation for location and day
+  # calculate distance from the earth to the sun, solar decimation, and sunset solar angle
   dr <- 1 + 0.033 * cos(J * ((2 * pi) / 365))
   sd <- 0.409 * sin(J * ((2 * pi) / 365) - 1.39)
   ws <- (pi/2) - atan((-tan(l) * tan(sd)) / (1 - ((tan(l))^2) * ((tan(sd))^2)^0.5))
+  # calculate extraterrestrial radiation
   Ra <- (24 * 60 / pi) * Gsc * dr * 
     (ws * sin(l) * sin(sd) + cos(l) * cos(sd) * sin(ws))
   N <- ws * (24 / pi)
+  # calculate shortwave radiation
   Rs <- ((as + bs * (ns / N)) * Ra)
   Rso <- (0.75 + (elev * 2e-5)) * Ra
+  # calculate net shortwave radiation
   Rns <- (1 - alb) * Rs
+  # calculate net longwave radiation
   Rnl <- (((Tmax + Tmin) / 2) * (0.34 - 0.14 * sqrt(ea)) * (1.35 * (Rs/Rso) - 0.35))
+  # calculate net radiation
   Rn_MJ <- (Rns + Rnl)
   # convert from MJ m-2 day-1 to W m-2
   Rn <- 11.82033 * Rn_MJ
   
-  # ground radiation, Eq. 23
+  # calculate ground radiation, Eq. 23
   G <- (Rn * 0.2)
 
-  # resistance terms
+  # calculate resistance terms
   rs <- exp(8.206 - 4.2555 * (SM / SMs))
   ra <- ((log10((x - d) / zs))^2) / ((k^2) * ux) # Eq. 25
 
-  # evapotranspiration
+  # calculate evapotranspiration using the Penman-Monteith equation
   ET <- (s * (Rn - G) + (p * Cp * D) / ra) / (s + y * (1 + rs / ra))
   
   # output
